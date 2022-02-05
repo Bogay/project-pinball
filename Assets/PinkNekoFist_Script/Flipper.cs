55af8da0d11eee56c28d2084e4cf79bd7f0d20dd
bogay <pojay11523@gmail.com>
2022-02-05 14:45:21 +0800

    fix(flipper): rotate around pivot

---
+++ Assets/PinkNekoFist_Script/Flipper.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    [SerializeField]
+    private float initialAngle;
+    [SerializeField]
+    private float finalAngle;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private KeyCode triggerKey;
+
+    private HingeJoint2D joint2D;
+
+    private void Start()
+    {
+        this.joint2D = GetComponent<HingeJoint2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        var motor = this.joint2D.motor;
+        var speed = Input.GetKey(this.triggerKey) ? this.speed : -this.speed;
+        motor.motorSpeed = speed;
+        this.joint2D.motor = motor;
+    }
+}
